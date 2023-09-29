fe76b3c0009eb52ce69451920a6dcce4a588237a
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-09-29 17:35:53 +0200

    Extract Simple Threads in own file

--- src/main/java/com/project/app/App.java
+++ src/main/java/com/project/app/App.java
@@ -2,41 +2,14 @@ package com.project.app;
 
 import com.project.app.HelloRunnable;
 import com.project.app.HelloThread;
-import com.project.app.SimpleThreads.MessageLoop;
-import static com.project.app.SimpleThreads.threadMessage;
+import com.project.app.SimpleThreads;
 
 public class App 
 {
     public static void main( String[] args ) throws InterruptedException
-    {       
-
-        long patience = 1 * 60 * 60; //patience of 3600 milliseconds
-
-        if (args.length > 0) {
-            try {
-                patience = Long.parseLong(args[0]) * 1000;
-            } catch (NumberFormatException ex) {
-                System.err.println("Argument must be an integer.");
-                System.exit(1);
-            }
-        }
-
-        threadMessage("Starting MessageLoop thread");
-        long startTime = System.currentTimeMillis();
-        Thread t = new Thread(new MessageLoop());
-        t.start();
-
-        threadMessage("Waiting for MessageLoop thread to finish");
-        while (t.isAlive()) {
-            threadMessage("Still waiting...");
-            t.join(1000);
-            if (((System.currentTimeMillis() - startTime) > patience) && t.isAlive()) {
-                threadMessage("Tired of waiting!");
-                t.interrupt();
-                t.join();
-            }
-        }
-        threadMessage("Finally!");
-
+    {   
+        (new Thread(new HelloRunnable())).start();   
+        new HelloThread().start();
+        SimpleThreads.execute();
     }
 }
--- src/main/java/com/project/app/SimpleThreads.java
+++ src/main/java/com/project/app/SimpleThreads.java
@@ -9,7 +9,7 @@ public class SimpleThreads {
                           message);
     }
 
-    public static class MessageLoop
+    private static class MessageLoop
         implements Runnable {
         public void run() {
             String importantInfo[] = {
@@ -22,7 +22,7 @@ public class SimpleThreads {
                 for (int i = 0;
                      i < importantInfo.length;
                      i++) {
-                    Thread.sleep(500); //change this to 4000 to see patience run out
+                    Thread.sleep(4000); //thread will take 4 secs to execute
                     threadMessage(importantInfo[i]);
                 }
             } catch (InterruptedException e) {
@@ -30,4 +30,45 @@ public class SimpleThreads {
             }
         }
     }
+
+    public static void execute() {
+        long patience = 1000 * 60 * 60; //patience of 1 hour
+
+        // if (args.length > 0) {
+        //     try {
+        //         patience = Long.parseLong(args[0]) * 1000;
+        //     } catch (NumberFormatException ex) {
+        //         System.err.println("Argument must be an integer.");
+        //         System.exit(1);
+        //     }
+        // }
+
+        threadMessage("Starting MessageLoop thread");
+        long startTime = System.currentTimeMillis();
+        Thread t = new Thread(new MessageLoop());
+        t.start();
+
+        threadMessage("Waiting for MessageLoop thread to finish");
+        while (t.isAlive()) {
+            threadMessage("Still waiting...");
+            try {
+                t.join(1000); //attempts to join every second so we should see 4 prints per thread
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            if (((System.currentTimeMillis() - startTime) > patience) && t.isAlive()) {
+                threadMessage("Tired of waiting!");
+                t.interrupt();
+                try {
+                    t.join();
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        threadMessage("Finally!");
+
+    }
+
 }
+
