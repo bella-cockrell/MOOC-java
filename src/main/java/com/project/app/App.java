9fd7b29e7f124e311a9da610dd6b03b54181083e
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-09-29 16:00:16 +0200

    Add SimpleThreads with join()

--- src/main/java/com/project/app/App.java
+++ src/main/java/com/project/app/App.java
@@ -2,14 +2,42 @@ package com.project.app;
 
 import com.project.app.HelloRunnable;
 import com.project.app.HelloThread;
-import com.project.app.SleepMessages.*;
+import com.project.app.SimpleThreads.MessageLoop;
+import static com.project.app.SimpleThreads.threadMessage;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        (new Thread(new HelloRunnable())).start(); 
+    public static void main( String[] args ) throws InterruptedException
+    {       
 
-        (new HelloThread()).start();        
+        long patience = 1000 * 60 * 60;
+
+        if (args.length > 0) {
+            try {
+                patience = Long.parseLong(args[0]) * 1000;
+            } catch (NumberFormatException ex) {
+                System.err.println("Argument must be an integer.");
+                System.exit(1);
+            }
+        }
+
+        //cannot compile
+        threadMessage("Starting MessageLoop thread");
+        long startTime = System.currentTimeMillis();
+        Thread t = new Thread(new MessageLoop());
+        t.start();
+
+        threadMessage("Waiting for MessageLoop thread to finish");
+        while (t.isAlive()) {
+            threadMessage("Still waiting...");
+            t.join(1000);
+            if (((System.currentTimeMillis() - startTime) > patience) && t.isAlive()) {
+                threadMessage("Tired of waiting!");
+                t.interrupt();
+                t.join();
+            }
+        }
+        threadMessage("Finally!");
+
     }
 }
